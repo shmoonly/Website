a1552525d9f9af460679a59dd6394f9778d4c567
Moon <149363332+shmoonly@users.noreply.github.com>
2024-12-03 01:41:09 +0000

    util.js

--- assets/js/util.js
+++ assets/js/util.js
@@ -1,9 +1,5 @@
 (function($) {
 
-	/**
-	 * Generate an indented list of links from a nav. Meant for use with panel().
-	 * @return {jQuery} jQuery object.
-	 */
 	$.fn.navList = function() {
 
 		var	$this = $(this);
@@ -34,11 +30,6 @@
 
 	};
 
-	/**
-	 * Panel-ify an element.
-	 * @param {object} userConfig User config.
-	 * @return {jQuery} jQuery object.
-	 */
 	$.fn.panel = function(userConfig) {
 
 		// No elements?
@@ -296,10 +287,6 @@
 
 	};
 
-	/**
-	 * Apply "placeholder" attribute polyfill to one or more forms.
-	 * @return {jQuery} jQuery object.
-	 */
 	$.fn.placeholder = function() {
 
 		// Browser natively supports placeholders? Bail.
@@ -518,11 +505,6 @@
 
 	};
 
-	/**
-	 * Moves elements to/from the first positions of their respective parents.
-	 * @param {jQuery} $elements Elements (or selector) to move.
-	 * @param {bool} condition If true, moves elements to the top. Otherwise, moves elements back to their original locations.
-	 */
 	$.prioritize = function($elements, condition) {
 
 		var key = '__prioritize';
